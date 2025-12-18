deda42714d2245dd2e34f9022e1ff7ce2294a44e
Tommy Prats <tommy.prats@etu.univ-grenoble-alpes.fr>
2025-12-18 03:10:31 +0100

    Some ideas in TODO

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -1,16 +1,28 @@
 #include "syscall.h"
+char *ma_chaine = "abcdefghijklmnopqrstuvwxyz";
 
 void super_fun_2(void *arg){
+    PutInt((int) arg);
+    PutChar('\n');
     ExitThread();
 }
 
 void super_fun_1(void *arg){
-    for (int i = 0; i < *(int *)arg; i++) { CreateThread(super_fun_2, arg); }
+    int *tmp = (int *) arg;
+    PutInt((int) arg);
+    PutChar('\n');
+    PutInt(*tmp);
+    PutChar('\n');
+    for (int i = 0; i < *tmp; i++) { 
+        int offset = i *4;
+        CreateThread(super_fun_2, (void *) (arg + offset));
+    }
     ExitThread();
 }
 
 int main(){
     int x = 3;
-    for (int i = 0; i < x; i++) { CreateThread(super_fun_1, &x); }
-    return 0;
+    int my_tab[] = {0,1,2,4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096,8192, 16384, 32768, 65536};
+    for (int i = 0; i < x; i++) { CreateThread(super_fun_1, (void *) &(my_tab[i])); }
 }
+
