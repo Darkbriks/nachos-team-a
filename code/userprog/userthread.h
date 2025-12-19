ab3912f7ffa7eafc8929dff53e8242ea199caad9
Alioune-diene <alioune.diene45@gmail.com>
2025-12-19 12:13:58 +0100

    Added automatic thread termination management in CreateThread and added tests to verify this functionality.

--- code/userprog/userthread.h
+++ code/userprog/userthread.h
@@ -9,9 +9,10 @@
  *
  * @param function  the funtion to executes
  * @param arg  a pointer for the argument of the function
+ * @param exit_addr  address of ExitThread function (for automatic termination)
  * @return the TID of the new thread. Return -1 if something bad happens
  */
-extern int do_UserThreadCreate(int function, int arg);
+extern int do_UserThreadCreate(int function, int arg, int exit_addr = 0);
 
 /**
  * @brief Destruct the caller thread 
@@ -28,10 +29,12 @@ class Param{
     private:
         ptr_32 function ;
         ptr_32 arg;
+        ptr_32 exit_addr;
     public:
         ptr_32 get_function() const{return function;}
         ptr_32 get_arg() const{return arg;}
-        Param(const ptr_32 f, const ptr_32 a){function=f; arg =a;}
+        ptr_32 get_exit_addr() const{return exit_addr;}
+        Param(const ptr_32 f, const ptr_32 a, const ptr_32 exit = 0){function=f; arg=a; exit_addr=exit;}
 };
 
 #endif //USERTHREAD__H
