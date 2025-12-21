13f39e0ab217c98527d99ede8486098079413267
Alioune-diene <alioune.diene45@gmail.com>
2025-12-21 14:07:52 +0100

    Added validation tests for semaphores and improved pointer handling in user code

--- code/userprog/userSem.cc
+++ code/userprog/userSem.cc
@@ -1,33 +1,94 @@
 #include "userSem.h"
 #include "system.h"
+#include "syscall.h"
+#include "linked_list.h"
+#include "synch.h"
 
+// Global list to track valid semaphores for security
+// Each process should have its own list, but for now we use a global one
+static LinkedList<Semaphore> *validSemaphores = nullptr;
+static Lock *semaphoreListLock = nullptr;
 
+static void InitSemaphoreTracking() {
+    if (validSemaphores == nullptr) {
+        validSemaphores = new LinkedList<Semaphore>();
+        semaphoreListLock = new Lock("semaphore list lock");
+    }
+}
 
-// TODO verify in P, V destroy Semaphore is a good pointer and user don't break kernel !
+static bool IsValidSemaphore(Semaphore *sem) {
+    if (sem == nullptr) return false;
+
+    semaphoreListLock->Acquire();
+    Semaphore *found = validSemaphores->FindInList(sem);
+    semaphoreListLock->Release();
+
+    return (found != nullptr);
+}
 
 void handle_SC_SemInit(){
     ASSERT(sizeof(int) == 4 && 4 == sizeof(Semaphore *));
+
+    InitSemaphoreTracking();
+
     int semUserAddr = machine->ReadRegister(4);
     int originalValue = machine->ReadRegister(5);
-    Semaphore *newSem = new Semaphore("sem_thread", originalValue);
-    machine->WriteMem(semUserAddr, sizeof(Semaphore *), (int) newSem);
+
+    if (semUserAddr < 0) {
+        RETURN(-E_FAULT);
+    }
+
+    if (originalValue < 0) {
+        RETURN(-E_INVAL);
+    }
+
+    Semaphore *newSem = new Semaphore("user_sem", originalValue);
+
+    semaphoreListLock->Acquire();
+    validSemaphores->AddInList(newSem);
+    semaphoreListLock->Release();
+
+    if (!machine->WriteMem(semUserAddr, sizeof(Semaphore *), (int) newSem)) {
+        semaphoreListLock->Acquire();
+        validSemaphores->RemoveInList(newSem);
+        semaphoreListLock->Release();
+        delete newSem;
+        RETURN(-E_FAULT);
+    }
+
+    RETURN(0);
 }
 
 void handle_common(int curCase){
     ASSERT(sizeof(int) == 4 && 4 == sizeof(Semaphore *));
     int addr = machine->ReadRegister(4);
-    int x;
+    if (addr < 0) {
+        RETURN(-E_FAULT);
+    }
 
-    machine->ReadMem(addr , sizeof(Semaphore *), &x);
-    Semaphore * sem = (Semaphore *) x;
+    int x;
+    if (!machine->ReadMem(addr, sizeof(Semaphore *), &x)) {
+        RETURN(-E_FAULT);
+    }
+
+    Semaphore *sem = (Semaphore *) x;
+
+    if (!IsValidSemaphore(sem)) {
+        RETURN(-E_INVAL);
+    }
 
     if (curCase == 0){
         sem->P();
     } else if (curCase == 1){
         sem->V();
     } else if (curCase == 2){
+        semaphoreListLock->Acquire();
+        validSemaphores->RemoveInList(sem);
+        semaphoreListLock->Release();
         delete sem;
     }
+    RETURN(0);
+
 }
 
 
