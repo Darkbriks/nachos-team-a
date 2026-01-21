4f504bba56e36d023b74cbb76569360941ba563b
Darkbriks <Darkbriks@users.noreply.github.com>
2026-01-21 08:38:22 +0000

    deploy: 46dbf2a2610976bfb1aa43bc155338b7201e19d7

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1d10b05f.js',
+                path_to_root + 'searchindex-5f6d642c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
