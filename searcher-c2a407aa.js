37496604e90a7197de5ec202479d9ac2e56c5a5a
Darkbriks <Darkbriks@users.noreply.github.com>
2026-01-22 10:58:37 +0000

    deploy: f6fadbf860923549131213755858234aaeeb77e0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a73492bf.js',
+                path_to_root + 'searchindex-71a727a8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
